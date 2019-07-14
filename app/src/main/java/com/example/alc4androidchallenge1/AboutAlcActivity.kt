6528f73dccd1b7efd7b674a9d14512bfa39c24b4
tobialbert <daadatobi@gmail.com>
2019-07-14 16:41:30 +0100

    Feat: Setup MainActivity

---
+++ app/src/main/java/com/example/alc4androidchallenge1/AboutAlcActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alc4androidchallenge1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutAlcActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_alc)
+    }
+}
--- app/src/main/java/com/example/alc4androidchallenge1/MainActivity.kt
+++ app/src/main/java/com/example/alc4androidchallenge1/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.alc4androidchallenge1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.reflect.KClass
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupUI()
     }
+
+    private fun setupUI() {
+        // setup click listeners
+        aboutAlcBtn.setOnClickListener { openActivity(AboutAlcActivity::class.java) }
+        myProfileBtn.setOnClickListener { openActivity(MyProfileActivity::class.java) }
+    }
+
+    private fun openActivity(activity: Class<*>) { startActivity(Intent(this, activity)) }
 }
